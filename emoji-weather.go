ba139312caf61049198782982a12e1d39b9c2cec
Justin Campbell <justin@justincampbell.me>
2014-12-05 15:03:13 -0500

    Use forecast package to retrieve icon

--- emoji-weather.go
+++ emoji-weather.go
@@ -1,13 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/justincampbell/forecast/v2"
+)
 
 var conditionIcons = map[string]string{
-	"clear-day": "☀️",
+	"clear-day":           "☀️",
+	"clear-night":         "🌙",
+	"cloudy":              "☁️",
+	"fog":                 "fog",
+	"partly-cloudy-day":   "⛅️",
+	"partly-cloudy-night": "🌙",
+	"rain":                "☔️",
+	"sleet":               "❄️ ☔️",
+	"snow":                "❄️",
+	"wind":                "🍃",
 }
 
 func main() {
-	fmt.Println(formatConditions("clear-day"))
+	key := os.Getenv("FORECAST_IO_API_KEY")
+	lat := "39.95"
+	long := "-75.1667"
+
+	res, err := forecast.GetResponse(key, lat, long, "now", "us")
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(formatConditions(extractConditionFromJSON(body)))
 }
 
 func formatConditions(condition string) (icon string) {
@@ -16,4 +45,13 @@ func formatConditions(condition string) (icon string) {
 		icon = condition
 	}
 	return
+}
+
+func extractConditionFromJSON(jsonBlob []byte) (condition string) {
+	f, err := forecast.FromJSON(jsonBlob)
+	if err != nil {
+		return "❗️"
+	}
+
+	return f.Currently.Icon
 }
--- emoji-weather_test.go
+++ emoji-weather_test.go
@@ -10,3 +10,17 @@ func Test_formatConditions(t *testing.T) {
 	assert.Equal(t, "☀️", formatConditions("clear-day"))
 	assert.Equal(t, "non-existent", formatConditions("non-existent"))
 }
+
+func Test_extractCloudyConditionFromJSON(t *testing.T) {
+	json := `{ "currently": { "icon": "cloudy" } }`
+	jsonBlob := []byte(json)
+
+	assert.Equal(t, "cloudy", extractConditionFromJSON(jsonBlob))
+}
+
+func Test_extractClearConditionFromJSON(t *testing.T) {
+	json := `{ "currently": { "icon": "clear-day" } }`
+	jsonBlob := []byte(json)
+
+	assert.Equal(t, "clear-day", extractConditionFromJSON(jsonBlob))
+}
