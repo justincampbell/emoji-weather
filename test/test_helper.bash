0f205cee7d7af39b6f7f0c73f16beffa6514b630
Justin Campbell <justin@justincampbell.me>
2014-12-08 17:40:13 -0500

    Extract fixture() for bats tests

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,8 +1,14 @@
 setup() {
+  ew="bin/emoji-weather -tmpdir test/tmp"
   mkdir -p test/tmp
 }
 
 teardown() {
   rm test/tmp/*.json
   echo $output
+}
+
+fixture() {
+  cp test/{fixtures,tmp}/$@
+  touch test/tmp/$@
 }
